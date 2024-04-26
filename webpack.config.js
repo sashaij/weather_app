46009721b5e7dac65b50941ed7a28d4ebe143a9b
sashaij <o.zagorodniuk@outlook.com>
2024-04-26 18:44:12 +0300

    Add image loader and babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,9 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
+        assetModuleFilename: '[name][ext]',
     },
+    devtool: 'source-map',
     module: {
         rules: [
             {
@@ -20,6 +22,20 @@ module.exports = {
                     'css-loader',
                     'sass-loader',
                 ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    }
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
             }
         ]
     },
