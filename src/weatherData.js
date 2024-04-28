2206ddb6cce51a4ffe91e8a93ea90273ccd4518e
sashaij <o.zagorodniuk@outlook.com>
2024-04-28 16:26:48 +0300

    Add forecast data

--- src/weatherData.js
+++ src/weatherData.js
@@ -1,10 +1,25 @@
 
 export default function getWeatherData (city) {
-    fetch(`https://api.weatherapi.com/v1/current.json?key=1ffba976248c430086994033241404&q=${city}`)
+
+    //current data and forecast
+    fetch(`https://api.weatherapi.com/v1/forecast.json?key=1ffba976248c430086994033241404&q=${city}&days=7`)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
+        console.log('forecast data:');
+
         console.log(data);
     })
+}
+
+function appWeatherData (apiData) {
+    let essentialData = {
+        
+    }
+
+    essentialData.condition = apiData.condition.code;
+    essentialData.temperature = apiData.temp_c;
+    essentialData.humidity = apiData.humidity;
+
 }
