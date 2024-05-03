bff9fb55f76fea68129ed8a1f59d3b9de70b83a1
sashaij <o.zagorodniuk@outlook.com>
2024-05-03 19:08:15 +0300

    Add function to display city from user input

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import './styles/main.scss';
 import getWeatherData from './weatherData';
+import { getInputData, handleClick } from './ui';
+
+
+const submitButton = document.getElementById('submit-button');
+
+function getWeather () {
+    let inputData;
+        inputData = getInputData();
+        console.log(inputData);
+        getWeatherData(inputData);
+}
+
+submitButton.addEventListener('click', handleClick)
+submitButton.addEventListener('click', getWeather)
+
