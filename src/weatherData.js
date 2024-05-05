31cf488c3062768e76915e8c4090d68412b71149
sashaij <o.zagorodniuk@outlook.com>
2024-05-05 15:12:13 +0300

    Rework weather data function

--- src/weatherData.js
+++ src/weatherData.js
@@ -1,6 +1,6 @@
 
-export default function getWeatherData (city) {
 
+function getWeatherData (city) {
     //current data and forecast
     fetch(`https://api.weatherapi.com/v1/forecast.json?key=1ffba976248c430086994033241404&q=${city}&days=8`)
     .then(function (response) {
@@ -11,11 +11,48 @@ export default function getWeatherData (city) {
 
         console.log(data);
 
-        currentWeatherData(data);
+        let essentialData = {
+        
+        }
+        //city name
+        essentialData.location = data.location.name;
+        //condition
+        essentialData.condition = data.current.condition.text;
+        //current temperature
+        essentialData.temperature = data.current.temp_c;
+        //highest temp
+        essentialData.highTemp = data.forecast.forecastday[0].day.maxtemp_c;
+        //lowest temp
+        essentialData.lowTemp = data.forecast.forecastday[0].day.mintemp_c;
+        //humidity
+        essentialData.humidity = data.current.humidity;
+        //sunrise
+        essentialData.sunrise = data.forecast.forecastday[0].astro.sunrise;
+        //sunset
+        essentialData.sunrise = data.forecast.forecastday[0].astro.sunset;
+        //chance of rain
+        essentialData.chanceOfRain = data.forecast.forecastday[0].day.daily_chance_of_rain;
+        //chance of snow
+        essentialData.chanceOfRain = data.forecast.forecastday[0].day.daily_chance_of_snow;
+        //wind speed
+        essentialData.wind_speed = data.current.wind_kph;
+        //wind direction
+        essentialData.wind_direction = data.current.wind_dir;
+        //percipitation
+        essentialData.visiblity = data.forecast.forecastday[0].day.totalprecip_mm;
+        //pressure
+        essentialData.pressure = data.current.pressure_mb;
+        //visibility
+        essentialData.visiblity = data.forecast.forecastday[0].day.avgvis_km;
+    
+    
+        console.log('current data: ');
+        console.log(essentialData);
     })
 }
 
-function currentWeatherData (apiData) {
+//essential data to be used
+/* function currentWeatherData (apiData) {
     let essentialData = {
         
     }
@@ -54,3 +91,7 @@ function currentWeatherData (apiData) {
     console.log('current data: ');
     console.log(essentialData);
 }
+ */
+
+
+export { getWeatherData }
