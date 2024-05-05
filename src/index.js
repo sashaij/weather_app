542f15355065c83fb055f76734239f46e82146c7
sashaij <o.zagorodniuk@outlook.com>
2024-05-05 15:14:17 +0300

    Rework get weather function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import './styles/main.scss';
-import getWeatherData from './weatherData';
+import { getWeatherData } from './weatherData';
 import { getInputData, handleClick } from './ui';
+
 
 
 const submitButton = document.getElementById('submit-button');
 
 function getWeather () {
     let inputData;
+        //get city name
         inputData = getInputData();
-        console.log(inputData);
-        getWeatherData(inputData);
+
+
+        getWeatherData(inputData)
 }
 
+//prevent form from submit
 submitButton.addEventListener('click', handleClick)
 submitButton.addEventListener('click', getWeather)
 
